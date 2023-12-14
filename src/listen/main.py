2026fc9ac9c19b05facb1f5cf47bca47e6b6ab6f
Laluka <louka.jacques-chevallier@manomano.com>
2023-12-14 17:40:49 +0100

    logger not logging

--- src/listen/main.py
+++ src/listen/main.py
@@ -16,10 +16,18 @@ Examples:
     listen some_doc.pdf
 """
 
+import logging
 from docopt import docopt
+from json import dumps
+logger = logging.getLogger(__name__)
+VERSION = "0.1.0"
 
 def main():
-    print("main")
+    logger.setLevel(logging.DEBUG)
+    logger.info("[+]Starting")
+    arguments = docopt(__doc__, version=f"listen {VERSION}")
+    logger.debug(f"Config: {dumps(arguments)}")
+    logger.info("Letzgooo")
 
 if __name__ == "__main__":
     main()
